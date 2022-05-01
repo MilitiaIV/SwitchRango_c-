10d07bcd8e8f12fe347c1333377fb15e0117f9a5
n7nx <72366290+MilitiaIV@users.noreply.github.com>
2022-04-30 23:41:35 -0500

    Create main.cpp
    range switch case example by n7nx

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]) {
+  char vocal;
+  for (int i=1;i<=3;i++)
+  {
+    int arr[] = { 'a','e','i','o','u'};
+   {
+  cout<<"\nIngrese la vocal: "; cin>>vocal;
+  switch (vocal)
+    {
+      case 'a' ... 'u': cout<<"Es una vocal minuscula.";break;
+      case 'A' ... 'U': cout<<"Es una vocal Mayuscula";break;
+      default: cout<<"Has ingresado un caracter erroneo, vuelve a intentar.";break;
+    }
+    cout<<"\n--------------------------------";
+   }
+  }
+    cout<<"\nProgramador: Antonio Gael Olivares Guerrero";
+    return 0;
+}
